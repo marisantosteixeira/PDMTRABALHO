0d720badbd5faf29c1e600b29b18cf80e4f21d2c
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-12-04 11:40:38 -0400

    colocando o trabalho no git

--- js/main.js
+++ js/main.js
@@ -1,49 +1,48 @@
 //registrando a service worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
-      try {
-        let reg;
-        reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
-  
-        console.log('Service worker registrada! 😎', reg);
-      } catch (err) {
-        console.log('😥 Service worker registro falhou: ', err);
-      }
-    });
-  }
-  
-  let posicaoInicial;
-   const capturarlocalizacao = document.getElementById ('localizacaoo');
-   const latitude = document.getElementById ('latitude');
-   const longitude = document.getElementById ('longitude');
-  
-   const sucesso =(posicao) => {
-    posicaoInicial =posicao;
-    latitude.innerHTML = posicaoInicial.coords.latitude
-    longitude.innerHTML = posicaoInicial.coords.longitude
-   };
-  
-   const erro =(error) => {
-    let errorMessage;
-    switch (error.code) {
-        case 0: 
-          errorMessage = "Erro desconhecido"
-        break;
-        case 1: 
-          errorMessage = "Permissão negada!"
-        break;
-        case 2: 
-          errorMessage = "Captura de posição indisponivel!"
-        break;
-        case 3: 
-          errorMessage = "Tempo de solicitação excedido!"
-        break;
-    
+  window.addEventListener('load', async () => {
+    try {
+      let reg;
+      reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+
+      console.log('Service worker registrada! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registro falhou: ', err);
     }
-    console.log('Ocorreu um erro:' + errorMessage);
-   };
+  });
+}
+
+let posicaoInicial;
+ const capturarlocalizacao = document.getElementById ('localizacaoo');
+ const latitude = document.getElementById ('latitude');
+ const longitude = document.getElementById ('longitude');
+
+ const sucesso =(posicao) => {
+  posicaoInicial =posicao;
+  latitude.innerHTML = posicaoInicial.coords.latitude
+  longitude.innerHTML = posicaoInicial.coords.longitude
+ };
+
+ const erro =(error) => {
+  let errorMessage;
+  switch (error.code) {
+      case 0: 
+        errorMessage = "Erro desconhecido"
+      break;
+      case 1: 
+        errorMessage = "Permissão negada!"
+      break;
+      case 2: 
+        errorMessage = "Captura de posição indisponivel!"
+      break;
+      case 3: 
+        errorMessage = "Tempo de solicitação excedido!"
+      break;
   
-   capturarlocalizacao.addEventListener('click', () => {
-    navigator.geolocation.getCurrentPosition(sucesso, erro);
-   })
-  
+  }
+  console.log('Ocorreu um erro:' + errorMessage);
+ };
+
+ capturarlocalizacao.addEventListener('click', () => {
+  navigator.geolocation.getCurrentPosition(sucesso, erro);
+ })
